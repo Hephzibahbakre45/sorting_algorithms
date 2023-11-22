4411f917b1a6243d1ec5e6de4fae44b2f33c38ed
hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-11-22 02:29:57 +0100

    task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+/**
+ * swap - the function that swaps 2 integer
+ * @array: array to be sorted
+ * @size: array length
+ * @a: first int
+ * @b: second int
+ * Return: void
+ */
+void swap(int *array, size_t size, int *a, int *b)
+{
+	if (*a != *b)
+	{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
+	}
+}
+
+/**
+ * lomuto_partition - order a subset of an array
+ * @array: array to be sorted
+ * @size: array length
+ * @lo: the low index of the range
+ * @hi: the high index of the range
+ *
+ * Return: void
+ */
+
+size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
+{
+	int i, j, pivot = array[hi];
+
+	for (i = j = lo; j < hi; j++)
+		if (array[j] < pivot)
+			swap(array, size, &array[j], &array[i++]);
+	swap(array, size, &array[i], &array[hi]);
+
+	return (i);
+}
+
+/**
+ * quicksort - sort using Lomuto partitioning scheme
+ * @array: array to be sorted
+ * @size: the array length
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
+ * Return: void
+ */
+void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
+{
+	if (lo < hi)
+	{
+		size_t p = lomuto_partition(array, size, lo, hi);
+
+		quicksort(array, size, lo, p - 1);
+		quicksort(array, size, p + 1, hi);
+	}
+}
+
+/**
+ * quick_sort - sorts an array of integers in
+ * ascending order using the Quick sort algorithm
+ * @array: array to be sorted
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+	quicksort(array, size, 0, size - 1);
+}
